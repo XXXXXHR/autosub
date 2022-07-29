057ba3bd82672d12844dcafb637002f5be3c3c9a
dsgfe <38750546+XXXXXHR@users.noreply.github.com>
2022-07-29 11:11:34 +0800

    Update autosub_v2b.py

--- autosub_v2b.py
+++ autosub_v2b.py
@@ -10,6 +10,7 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101
 home_urls = (
     'https://www.ckcloud.xyz',
     'https://baipiao.buzz',
+    'https://sjssr.fun',
 )
 times = 6
 proxies = {
